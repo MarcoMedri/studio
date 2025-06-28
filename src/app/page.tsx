d1dee1a8d6849852823a3fede93d2c8d8b089bb1
Marco Medri <marcun.medri@gmail.com>
2025-06-28 07:22:57 +0000

    aggiunto supbase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { JournalLayout } from '@/components/journal-layout';
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 
 export default function Home() {
+  return ( <Page /> )
+}
+
+async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos, error } = await supabase.from('todos').select('*');
+
+  if (error) {
+    console.error("Error fetching todos:", error);
+    return <div>Errore nel caricamento dei todo.</div>;
+  }
+
   return (
-    <main>
-      <JournalLayout />
-    </main>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos?.map((todo: any) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
